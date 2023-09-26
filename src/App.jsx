56121f5ea9154c851bef6f32043339fdee836619
Lawrence Caulker <locaulker@gmail.com>
2023-09-26 01:40:17 -0400

    implemented lorem ipsum functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ const App = () => {
   const [count, setCount] = useState(1)
   const [text, setText] = useState([])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    let amount = parseInt(count)
+    setText(data.slice(0, amount))
+  }
+
   return (
     <section className="section-center">
       <h4>Tired of boring Lorem Ipsum?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs:</label>
         <input
           type="number"
@@ -24,6 +31,11 @@ const App = () => {
           Generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>
+        })}
+      </article>
     </section>
   )
 }
