2b449c3501e31c6b9e1a411b84360ad59fc45357
Lawrence Caulker <locaulker@gmail.com>
2023-09-26 00:57:22 -0400

    import data.js and setup text state value

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react"
+import data from "./data"
 
 const App = () => {
   const [count, setCount] = useState(1)
+  const [text, setText] = useState([])
 
-  return <h2>Lorem Ipsum Starter</h2>
+  return (
+    <section className="section-center">
+      <h4>Tired of boring Lorem Ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+    </section>
+  )
 }
 export default App
